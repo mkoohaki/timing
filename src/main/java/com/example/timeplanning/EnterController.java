3c044716f0f48ea89e0364be9c8b59cdecb9a00f
mkoohaki <76530864+mkoohaki@users.noreply.github.com>
2022-09-20 20:55:46 -0400

    Clock

--- src/main/java/com/example/timeplanning/EnterController.java
+++ src/main/java/com/example/timeplanning/EnterController.java
@@ -1,12 +1,14 @@
 package com.example.timeplanning;
 
 import com.example.timeplanning.database.AccountDatabase;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.stage.Window;
 
-import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
@@ -18,24 +20,26 @@ public class EnterController implements Initializable {
     @FXML
     private Label time, date, day, status;
 
+    private volatile boolean stop;
+
     @FXML
     protected void setPlan(ActionEvent event) throws Exception {
         Partials.windowOpen("schedule", "Timing Plan", 620, 320);
+        stop = true;
         Partials.windowClose(event);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        DateTimeFormatter timing = DateTimeFormatter.ofPattern("HH:mm:ss");
         DateTimeFormatter weekDay = DateTimeFormatter.ofPattern("EEEE");
         DateTimeFormatter dating = DateTimeFormatter.ofPattern("MMM.dd, yyyy");
         LocalDateTime now = LocalDateTime.now();
 
         day.setText(weekDay.format(now));
         date.setText(dating.format(now));
-        time.setText(timing.format(now).substring(0, 5));
 
+        Timenow();
         try {
             AccountDatabase db = new AccountDatabase();
             String[][] rows = db.getAllRows();
@@ -55,5 +59,32 @@ public class EnterController implements Initializable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    //Updating time
+    private void Timenow() {
+        Thread clock = new Thread( () -> {
+            DateTimeFormatter timing = DateTimeFormatter.ofPattern("HH:mm:ss");
+            while(!stop) {
+                try{
+                    System.out.println("S");
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                LocalDateTime now = LocalDateTime.now();
+                final String timenow = timing.format(now).substring(0, 8);
+                Platform.runLater( () -> {
+                    time.setText(timenow);
+                });
+            }
+        });
+        clock.start();
+    }
+
+    @FXML
+    public void exitApplication(ActionEvent event) {
+        stop = true;
+        Platform.exit();
     }
 }
